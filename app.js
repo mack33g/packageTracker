a7bbc120b83fe44c58581dfdbc511bd1db3a3459
mack33g <leogong08@gmail.com>
2020-12-19 01:37:35 -0800

    fix playwright browser assignment on mac

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ function identifyCarrier(trackingId) {
 }
 
 async function checkPackage(package, carrier) {
-    const browser = (appConfig.environment == 'test') ? await playwright.chromium.launch({ 
+    const browser = (appConfig.environment == 'mac') ? await playwright.chromium.launch({ 
         // headless: false
     }) : await playwright.chromium.launch({executablePath: '/usr/bin/chromium-browser' });
     const context = await browser.newContext(); 
