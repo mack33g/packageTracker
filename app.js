03eb60a237eae3be2258580e847828dcfa4b5b38
mack33g <leogong08@gmail.com>
2020-12-22 00:22:27 -0800

    split out carrier configs into a separate file so that gmail.js can use it

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mysql = require("mysql");
 // var gmail = require("./gmail");
 const appConfig = require("./config");
 const playwright = require("playwright");
+const carrierConfigs = require("./carrierConfigs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,66 +21,6 @@ con.connect(function(err) {
 if (err) throw err;
 console.log("Connected to mySQL!");
 });
-
-const carrierConfigs = {
-    ups: {
-        name: 'ups',
-        url: 'https://www.ups.com/WebTracking?loc=en_US&trackNums=',
-        status: {
-            selector: '[id^="stApp_"]',
-            index: [12]
-        },
-        details: {
-            selector: '[id^="stApp_"]',
-            index: [13]
-        },
-        // Making this a string pattern as opposed to a regex expression so that I can use this in regex
-        // constructors like when looking for tracking numbers in gmail
-        pattern: [
-            '(1Z)[0-9A-Z]{16}',
-            '(T)+[0-9A-Z]{10}',
-            '[0-9]{9}',
-            '[0-9]{26}'
-        ]
-    },
-    fedex: {
-        name: 'fedex',
-        url: 'http://www.fedex.com/Tracking?&tracknumbers=',
-        status: {
-            selector: '.redesignSnapshotTVC.snapshotController_addr_label.dest',
-            index: [0]
-        },
-        details: {
-            selector: '.redesignSnapshotTVC.snapshotController_date.dest',
-            index: [0]
-        },
-        pattern: [
-            '[0-9]{20}',
-            '[0-9]{15}',
-            '[0-9]{12}',
-            '[0-9]{22}'
-        ]
-    },
-    usps: {
-        name: 'usps',
-        url: 'https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=',
-        status: {
-            selector: '.expected_delivery',
-            index: [0]
-        },
-        details: {
-            selector: '.delivery_status',
-            index: [0]
-        },
-        pattern: [
-            '(94|93|92|94|95)[0-9]{20}',
-            '(94|93|92|94|95)[0-9]{22}',
-            '(70|14|23|03)[0-9]{14}',
-            '(M0|82)[0-9]{8}',
-            '([A-Z]{2})[0-9]{9}([A-Z]{2})'
-        ]
-    }
-};
 
 app.get("/", function(req, res) {
     res.render("home.ejs");
